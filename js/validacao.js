78bc5e03be99cd23b95b8f0e28522b2ebeb0a7af
ikyrie <mah.c5r@gmail.com>
2020-11-03 11:15:17 -0300

    aula2 video2.1

--- js/validacao.js
+++ js/validacao.js
@@ -7,14 +7,53 @@ export function valida(input) {
 
     if(input.validity.valid) {
         input.parentElement.classList.remove('input-container--invalido')
+        // input.parentElement.querySelector('span').innerHTML = ''
     } else {
         input.parentElement.classList.add('input-container--invalido')
+        // input.parentElement.querySelector('span').innerHTML = pegaMensagemDeErro(tipoDeInput, input)
     }
 }
 
 const validadores = {
     dataNascimento: input => validaDataNascimento(input)
 }
+
+const tiposDeErro = [
+    "valueMissing",
+    "customError",
+    "typeMismatch",
+    "valueMissing",
+    "tooShort"
+]
+
+const mensagensDeErro = {
+    dataNascimento: {
+        valueMissing: "A data não pode estar vazia",
+        customError: "Você deve ser maior de 18 anos para se cadastrar"
+    },
+    email: {
+        valueMissing: "O email não pode estar vazio",
+        typeMismatch: "O email digitado não é válido"
+    },
+    senha: {
+        valueMissing: "A senha não pode estar vazia",
+        tooShort: "A senha é muito curta",
+        typeMismatch: "A senha deve conter entre 8 e 12 caractéres e deve conter um número e uma letra maiúscula."
+    },
+    nome: {
+        valueMissing: "O nome não pode estar vazio"
+    }
+}
+
+// function pegaMensagemDeErro(tipoDeInput, input) {
+//     let mensagem = ''    
+//     tiposDeErro.forEach(erro => {
+//         if(input.validity[erro]) {
+//             mensagem = mensagensDeErro[tipoDeInput][erro]
+//         }
+//     })
+//     return mensagem
+// }
 
 function validaDataNascimento(input) {
     const dataRecebida = new Date(input.value)
